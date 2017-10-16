a615fd73340fbd5e367f761bc778ff7c06b12e63
Maarten Smeets <maarten.smeets@amis.nl>
2017-10-16 20:46:05 +0200

    Update for XE installation

--- provision.sh
+++ provision.sh
@@ -1,5 +1,10 @@
 yum -y install oracle-rdbms-server-11gR2-preinstall
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+yum -y install gparted
 cd /software
-unzip oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+unzip -o oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 cd Disk1
 rpm -ivh oracle-xe-11.2.0-1.0.x86_64.rpm
+/etc/init.d/oracle-xe configure responseFile=../xe.rsp
+
