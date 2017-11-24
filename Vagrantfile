2e4bd317326309dbfa0d7e509ad52f81084ef8b0
Maarten Smeets <wisp1980@gmail.com>
2017-11-24 15:38:08 +0100

    Updating provisioning files

--- Vagrantfile
+++ Vagrantfile
@@ -20,18 +20,23 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   soadb.vm.box_version = "0.0.1"
 	
   #for local
+  #first do something like: vagrant box add ol74 file:///d:/vagrant/packer/virtualbox/ol74.box
+  #for new versions first vagrant box remove ol74
   #soadb.vm.box = "ol74"
+  
   soadb.vm.hostname = "soadb"
     
   #update this folder. it should contain oracle-xe-11.2.0-1.0.x86_64.rpm.zip and xe.rsp
-  soadb.vm.synced_folder "d:/vagrant/software", "/software", :mount_options => ["dmode=777","fmode=777"]
+  soadb.vm.synced_folder "./software", "/software", :mount_options => ["dmode=777","fmode=777"]
 
-  soadb.vm.network :private_network, ip: "192.168.0.1"
+  soadb.vm.network :private_network, ip: "192.168.3.2"
 
   soadb.vm.provider :virtualbox do |vb|
-      vb.customize ["modifyvm"     , :id, "--memory", "4096"]
+      vb.customize ["modifyvm"     , :id, "--memory", "8192"]
       vb.customize ["modifyvm"     , :id, "--name"  , "soadb"]
-      vb.customize ["modifyvm"     , :id, "--cpus"  , 4]
+      vb.customize ["modifyvm"     , :id, "--cpus"  , 2]
+	  vb.customize ["modifyvm"     , :id, "--clipboard","bidirectional"]
+	  vb.customize ["modifyvm"     , :id, "--vram","16"]
   end
 	
   #this starts provisioning the machine
--- provision.sh
+++ provision.sh
@@ -1,18 +1,16 @@
 #Useful for user oracle
-echo 'oracle  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+#Already present in provided Packer template
+#echo 'oracle  ALL=(ALL:ALL) ALL' >> /etc/sudoers
 usermod -a -G vboxsf oracle
 
-#Useful extra tools
 yum -y update
-yum -y install gparted
 cd /software
 
-#XE database
-yum -y install oracle-rdbms-server-11gR2-preinstall
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
-rpm -ivh oracle-xe-11.2.0-1.0.x86_64.rpm
-/etc/init.d/oracle-xe configure responseFile=xe.rsp
+yum -y install gparted
+
+
 
 #Java
 yum -y remove java*
@@ -24,8 +22,43 @@ alternatives --install /usr/bin/jar jar /usr/java/jdk1.8.0/bin/jar 20000
 alternatives --install /usr/bin/javac javac /usr/java/jdk1.8.0/bin/javac 20000
 alternatives --install /usr/bin/javaws javaws /usr/java/jdk1.8.0/jre/bin/javaws 20000
 
+#Database
+#rpm -ivh oracle-xe-11.2.0-1.0.x86_64.rpm
+#/etc/init.d/oracle-xe configure responseFile=xe.rsp
+yum -y install oracle-database-server-12cR2-preinstall
+sed -i '1s/^/192.168.3.2 soadb\n/' /etc/hosts
+
+unzip -o linuxx64_12201_database.zip
+runuser -l oracle -c '/software/database/runInstaller -waitforcompletion -silent -invPtrLoc /software/inv.loc -responseFile /software/db.rsp'
+printf '\n\n' | /home/oracle/app/oracle/product/12.2.0/dbhome_1/root.sh
+
+runuser -l oracle -c '/software/database/runInstaller -executeConfigTools -waitforcompletion -silent -invPtrLoc /software/inv.loc -responseFile /software/db.rsp'
+
+sed -i 's/dbhome_1:N/dbhome_1:Y/g' /etc/oratab
+#ORCL:/home/oracle/app/oracle/product/12.2.0/dbhome_1:Y in /etc/oratab
+
+#create dbora script
+cp /software/dbora /etc/init.d
+chmod 750 dbora
+ln -s /etc/init.d/dbora /etc/rc.d/rc0.d/K01dbora
+ln -s /etc/init.d/dbora /etc/rc.d/rc3.d/S99dbora
+ln -s /etc/init.d/dbora /etc/rc.d/rc5.d/S99dbora
+
 #WebLogic
-unzip V886423-01.zip
+unzip -o V886423-01.zip
 java -jar fmw_12.2.1.3.0_wls.jar -silent -invPtrLoc `pwd`/inv.loc -responseFile `pwd`/wls.rsp
 
-#unzip V886426-01.zip
+#SOA Infrastructure
+unzip -o V886426-01.zip
+java -jar fmw_12.2.1.3.0_infrastructure.jar -silent -invPtrLoc `pwd`/inv.loc -responseFile `pwd`/wls_infra.rsp
+
+#SOA Suite
+unzip -o V886440-01.zip
+java -jar fmw_12.2.1.3.0_soa.jar -silent -invPtrLoc `pwd`/inv.loc -responseFile `pwd`/soa.rsp
+
+#OSB
+unzip -o V886445-01.zip
+java -jar fmw_12.2.1.3.0_osb.jar -silent -invPtrLoc `pwd`/inv.loc -responseFile `pwd`/osb.rsp
+
+#RCU
+/home/oracle/Oracle/Middleware1221/oracle_common/bin/rcu -silent -responseFile `pwd`/rcuResponseFile.properties
