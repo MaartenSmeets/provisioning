ceb8342b2e405732b60918d25cec0a323e4224ad
Maarten Smeets <maarten.smeets@amis.nl>
2017-12-29 10:28:11 +0100

    Adding Docker configuration

--- provision.sh
+++ provision.sh
@@ -6,11 +6,21 @@ usermod -a -G vboxsf oracle
 yum -y update
 cd /software
 
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -ivh epel-release-latest-7.noarch.rpm
-yum -y install gparted
+#wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#rpm -ivh epel-release-latest-7.noarch.rpm
+#yum -y install gparted
 
+#Docker
+yum-config-manager --enable ol7_addons
+yum-config-manager --enable ol7_optional_latest
+yum install docker-engine btrfs-progs btrfs-progs-devel -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker oracle
+docker login -u maarten.smeets@amis.nl -p XXX container-registry.oracle.com
 
+#Database
+docker run -d --env-file /software/db_env.dat -p 1521:1521 -p 5500:5500 -it --name dockerDB --shm-size="2g" container-registry.oracle.com/database/enterprise:12.2.0.1-slim
 
 #Java
 yum -y remove java*
@@ -21,28 +31,6 @@ alternatives --install /usr/bin/java java /usr/java/jdk1.8.0/jre/bin/java 20000
 alternatives --install /usr/bin/jar jar /usr/java/jdk1.8.0/bin/jar 20000
 alternatives --install /usr/bin/javac javac /usr/java/jdk1.8.0/bin/javac 20000
 alternatives --install /usr/bin/javaws javaws /usr/java/jdk1.8.0/jre/bin/javaws 20000
-
-#Database
-#rpm -ivh oracle-xe-11.2.0-1.0.x86_64.rpm
-#/etc/init.d/oracle-xe configure responseFile=xe.rsp
-yum -y install oracle-database-server-12cR2-preinstall
-sed -i '1s/^/192.168.3.2 soadb\n/' /etc/hosts
-
-unzip -o linuxx64_12201_database.zip
-runuser -l oracle -c '/software/database/runInstaller -waitforcompletion -silent -invPtrLoc /software/inv.loc -responseFile /software/db.rsp'
-printf '\n\n' | /home/oracle/app/oracle/product/12.2.0/dbhome_1/root.sh
-
-runuser -l oracle -c '/software/database/runInstaller -executeConfigTools -waitforcompletion -silent -invPtrLoc /software/inv.loc -responseFile /software/db.rsp'
-
-sed -i 's/dbhome_1:N/dbhome_1:Y/g' /etc/oratab
-#ORCL:/home/oracle/app/oracle/product/12.2.0/dbhome_1:Y in /etc/oratab
-
-#create dbora script
-cp /software/dbora /etc/init.d
-chmod 750 dbora
-ln -s /etc/init.d/dbora /etc/rc.d/rc0.d/K01dbora
-ln -s /etc/init.d/dbora /etc/rc.d/rc3.d/S99dbora
-ln -s /etc/init.d/dbora /etc/rc.d/rc5.d/S99dbora
 
 #WebLogic
 runuser -l oracle -c 'unzip -o V886423-01.zip'
@@ -61,4 +49,4 @@ runuser -l oracle -c 'unzip -o V886445-01.zip'
 runuser -l oracle -c 'java -jar fmw_12.2.1.3.0_osb.jar -silent -invPtrLoc `pwd`/inv.loc -responseFile `pwd`/osb.rsp'
 
 #RCU
-runuser -l oracle -c '/home/oracle/Oracle/Middleware1221/oracle_common/bin/rcu -silent -responseFile `pwd`/rcuResponseFile.properties'
+#runuser -l oracle -c '/home/oracle/Oracle/Middleware1221/oracle_common/bin/rcu -silent -responseFile `pwd`/rcuResponseFile.properties'
