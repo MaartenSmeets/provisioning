5d1369e586d27723cea8474d6eeba9bab04c6772
MaartenSmeets <wisp1980@gmail.com>
2018-02-02 16:41:35 +0100

    Updated base image. Now also ends with starting Kafka (docker-compose)

--- ubuntu/provision.sh
+++ ubuntu/provision.sh
@@ -1,42 +1,29 @@
+#Desktop
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ 
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository ppa:webupd8team/java
+
+apt-get update
+apt-get -y install aptitude apt-transport-https ca-certificates curl software-properties-common docker-ce docker-compose terminator firefox
+aptitude -y install --without-recommends ubuntu-desktop 
+
+#course user
 useradd -d /home/course -m course
-echo Welcome01 | passwd course --stdin
+echo -e "Welcome01\nWelcome01" | passwd course
 usermod -a -G vboxsf course
+usermod -a -G docker course
+usermod -a -G sudo course
 
 cd /software
-
-#wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-#rpm -ivh epel-release-latest-7.noarch.rpm
-#yum -y install gparted
-
-#Docker
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-	
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-   
-apt-get install docker-ce
-
-#Desktop
-apt-get install aptitude
-aptitude install --without-recommends ubuntu-desktop
-
-apt-get install terminator
 
 #JDK
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-  
-add-apt-repository ppa:webupd8team/java
+apt-get -y install oracle-java8-installer
 
-apt-get install oracle-java8-installer
+sudo -u course git clone https://github.com/wurstmeister/kafka-docker.git /home/course/kafka-docker
+sudo -u course -- sh -c "cd /home/course/kafka-docker; docker-compose -f docker-compose-single-broker.yml up -d"
 
-apt-get install system-config-kickstart
+#shutdown -P now
 
