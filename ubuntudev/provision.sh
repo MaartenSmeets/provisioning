0e5c0f0be1d8f14a674f733e04900c33b6b864d1
Maarten Smeets <wisp1980@gmail.com>
2018-08-24 10:01:22 +0200

    Quick and simple base dev VM

---
+++ ubuntudev/provision.sh
@@ -0,0 +1,35 @@
+#Desktop
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ 
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository ppa:webupd8team/java
+
+apt-get update
+apt-get -y install aptitude apt-transport-https ca-certificates curl software-properties-common docker-ce docker-compose libxss1 libgconf2-4
+aptitude -y install --without-recommends ubuntu-desktop 
+#Fix root not allowed to start X-window
+xhost local:root
+apt-get -y install terminator firefox gparted
+
+#developer user
+useradd -d /home/developer -m developer
+echo -e "Welcome01\nWelcome01" | passwd developer
+usermod -a -G vboxsf developer
+usermod -a -G docker developer
+usermod -a -G sudo developer
+usermod --shell /bin/bash developer
+
+#JDK
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get -y install oracle-java8-installer
+
+#Fix screen flickering issue
+sudo perl -e '$^I=".backup";while(<>){s/#(WaylandEnable=false)/$1/;print;}' /etc/gdm3/custom.conf
+
+#Hide vagrant
+echo '[User]' > /var/lib/AccountsService/users/vagrant
+echo 'SystemAccount=true' >> /var/lib/AccountsService/users/vagrant
+
+apt-get clean
+shutdown now -h
