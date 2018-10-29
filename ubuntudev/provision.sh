6c4ecd5efece3f1ac869977ee2f11aa9c5d06e08
Maarten Smeets <wisp1980@gmail.com>
2018-10-29 16:12:53 +0100

    Updating

--- ubuntudev/provision.sh
+++ ubuntudev/provision.sh
@@ -2,14 +2,14 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
  
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:linuxuprising/java
 
 apt-get update
 apt-get -y install jq aptitude apt-transport-https ca-certificates curl software-properties-common docker-ce docker-compose libxss1 libgconf2-4
 aptitude -y install --without-recommends ubuntu-desktop 
 #Fix root not allowed to start X-window
 xhost local:root
-apt-get -y install terminator firefox gparted
+apt-get -y install terminator firefox
 
 #developer user
 useradd -d /home/developer -m developer
@@ -22,7 +22,8 @@ usermod --shell /bin/bash developer
 #JDK
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get -y install oracle-java8-installer
+apt-get -y install oracle-java10-installer
+apt-get -y install oracle-java10-set-default
 
 #Fix screen flickering issue
 sudo perl -e '$^I=".backup";while(<>){s/#(WaylandEnable=false)/$1/;print;}' /etc/gdm3/custom.conf
@@ -43,8 +44,8 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
 
 #Kafka
-sudo -u developer wget http://www.kafkatool.com/download2/kafkatool.sh -O /home/developer/kafkatool.sh
-sudo -u developer git clone https://github.com/confluentinc/kafka-workshop.git /home/developer/kafka-workshop
+#sudo -u developer wget http://www.kafkatool.com/download2/kafkatool.sh -O /home/developer/kafkatool.sh
+#sudo -u developer git clone https://github.com/confluentinc/kafka-workshop.git /home/developer/kafka-workshop
 
 #Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -58,6 +59,15 @@ wget https://dl.pstmn.io/download/latest/linux64 -O /tmp/postman.tar.gz
 sudo tar -xzf /tmp/postman.tar.gz -C /opt
 rm /tmp/postman.tar.gz
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+#Soap UI
+#wget https://s3.amazonaws.com/downloads.eviware/soapuios/5.4.0/SoapUI-x64-5.4.0.sh
+
+#STS
+sudo -u developer wget https://download.springsource.com/release/STS/3.9.6.RELEASE/dist/e4.9/spring-tool-suite-3.9.6.RELEASE-e4.9.0-linux-gtk-x86_64.tar.gz -O /home/developer/sts.tar.gz
+sudo -u developer -- sh -c "cd /home/developer; tar xvfz sts.tar.gz"
+sudo -u developer -- sh -c "rm /home/developer/sts.tar.gz"
+
 
 apt-get autoremove
 apt-get clean
